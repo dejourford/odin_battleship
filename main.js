74a852b85ab0f9f68a86e413942412df002b0aa9
De'Jour <dejourford@yahoo.com>
2026-03-09 10:52:06 -0500

    create receive attack method for gameboard

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 import { Ship } from "./src/Ship.js";
+import { Gameboard } from "./src/Gameboard.js";
 
-const ship1 = new Ship(0);
-console.log(ship1.length)
+
+const newBoard = new Gameboard();
+const ship1 = new Ship(3);
+newBoard.placeShip(ship1, [[1,3], [1, 4], [1, 5]])
+ship1.hit();
+ship1.hit();
+newBoard.receiveAttack(1, 3)
+
+console.log('ships:', newBoard.ships)
+console.log(`${ship1}`, ship1.hits)
+console.log(ship1.isSunk());
--- src/Gameboard.js
+++ src/Gameboard.js
@@ -13,8 +13,21 @@ export class Gameboard {
     }
     // 2. NEEDS TO RECEIVE ATTACKS
     receiveAttack(x, y) {
-        const xCoordinate = x;
-        const yCoordinate = y;
+        
+        // loop ships array 
+        for (const entry of this.ships) {
+            for (const coord of entry.coordinates) {
+                if (coord[0] === x && coord[1] === y) {
+                    entry.ship.hit();
+                    return "hit"
+                }
+            }
+        }
+       
+        // track misses
+        this.missedAttacks.push([x, y]);
+        return "miss"
+        
     }
     
     // 4. NEEDS TO RETURN TRUE OR FALSE IF ALL SHIPS ARE SUNK
