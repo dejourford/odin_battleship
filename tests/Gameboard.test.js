080d7214610996f744adc56393a4e1dc4f48b708
De'Jour <dejourford@yahoo.com>
2026-03-09 10:51:49 -0500

    create test to check if gameboard tracks misses

--- tests/Gameboard.test.js
+++ tests/Gameboard.test.js
@@ -1,6 +1,7 @@
 import { Gameboard } from "../src/Gameboard";
 import { Ship } from "../src/Ship";
 
+// test gameboard stores ships
 test("gameboard stores ships", () => {
     const newBoard = new Gameboard();
     const ship = new Ship(3);
@@ -12,4 +13,22 @@ test("gameboard stores ships", () => {
             coordinates: [[1,3], [1, 4], [4, 5]]
         }
     ])
+})
+
+// test gameboard receives attacks
+test("gameboard receives attacks", () => {
+    const newBoard = new Gameboard();
+    const ship = new Ship(3);
+
+    newBoard.placeShip(ship, [[1, 3], [1, 4], [1, 5]])
+    newBoard.receiveAttack(1,3)
+    expect(ship.hits).toBe(1)
+})
+
+// test gameboard to track misses
+test("gameboard tracks misses", () => {
+    const newBoard = new Gameboard();
+   
+    newBoard.receiveAttack(1,6)
+    expect(newBoard.missedAttacks).toEqual([[1, 6]])
 })
