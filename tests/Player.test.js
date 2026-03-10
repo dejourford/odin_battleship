669d45496ee102e1ad9b598185cdda61e0262f03
De'Jour <dejourford@yahoo.com>
2026-03-09 20:18:54 -0500

    create test case for user attack against cpu and other users

--- tests/Player.test.js
+++ tests/Player.test.js
@@ -7,3 +7,31 @@ test("Player class creates player correctly", () => {
     expect(user.type).toBe("user");
     expect(user.board).toBeDefined();
 });
+
+// test whether Player class creates cpu correctly
+test("Player class creates player correctly", () => {
+    const user = new Player("cpu");
+
+    expect(user.type).toBe("cpu");
+    expect(user.board).toBeDefined();
+});
+
+// test whether player can attack cpu board
+test("Player can attack cpu board", () => {
+    const user = new Player("user");
+    const cpu = new Player("cpu")
+
+    user.attack(cpu.board, 3, 4);
+
+    expect(cpu.board.missedAttacks).toContainEqual([3,4]);
+})
+
+// test whether player can attack another player board
+test("Player can attack user board", () => {
+    const user = new Player("user");
+    const user2 = new Player("user")
+
+    user.attack(user2.board, 3, 4);
+
+    expect(user2.board.missedAttacks).toContainEqual([3,4]);
+})
