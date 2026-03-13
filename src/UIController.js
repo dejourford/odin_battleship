bb5aa2218c3df193d7b281a9e592041ff5d28865
De'Jour <dejourford@yahoo.com>
2026-03-13 00:45:06 -0500

    modify render board function to display default board if empty array is passed and to display the current user board otherwise

--- src/UIController.js
+++ src/UIController.js
@@ -11,32 +11,55 @@ export function renderBoard(board) {
     // set innerHTML to empty string
     app.innerHTML = "";
 
+    // if empty array is passed, then render empty board
+    if (board = []) {
+        const labelCollection = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+        for (let i = 1; i < 11; i++) {
 
-    // for loop for board length
-    const labelCollection = ["A","B","C","D","E","F","G","H","I","J"];
-    for (let i = 1; i < board.size + 1; i++) {
+            for (let j = 1; j < 11; j++) {
+                // build grid of cells
+                const cell = document.createElement("div");
+                cell.classList.add("cell");
 
-        for (let j = 1; j < board.size + 1; j++) {
-            // build grid of cells
-            const cell = document.createElement("div");
-            cell.classList.add("cell");
+                // give data attributes that represent cell coordinates
+                cell.dataset.x = i;
+                cell.dataset.y = j;
 
-            // give data attributes that represent cell coordinates
-            cell.dataset.x = i;
-            cell.dataset.y = j;
-            
-            // visible label
-            cell.textContent = `${labelCollection[j - 1]}${i}`
-            
-            boardElement.append(cell);
+                // visible label
+                cell.textContent = `${labelCollection[j - 1]}${i}`
 
+                boardElement.append(cell);
+
+
+            }
 
         }
-
     }
 
-    // append labels to grid
-
+    // for loop for board length
+    if (board.size) {
+        const labelCollection = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+        for (let i = 1; i < board.size + 1; i++) {
+    
+            for (let j = 1; j < board.size + 1; j++) {
+                // build grid of cells
+                const cell = document.createElement("div");
+                cell.classList.add("cell");
+    
+                // give data attributes that represent cell coordinates
+                cell.dataset.x = i;
+                cell.dataset.y = j;
+    
+                // visible label
+                cell.textContent = `${labelCollection[j - 1]}${i}`
+    
+                boardElement.append(cell);
+    
+    
+            }
+    
+        }
+    }
 
     // assembly
     app.append(boardElement);
