00c99e483ba94f53a33e6985ec15952082801479
De'Jour <dejourford@yahoo.com>
2026-03-13 00:19:47 -0500

    create gameboard and cell labels

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+import { Gameboard } from "./src/Gameboard.js";
 import { GameController } from "./src/GameController.js";
-
+import { renderBoard } from "./src/UIController.js";
 
 const game = new GameController();
+
+
+renderBoard(game.currentPlayer.board);
+
+
 game.setupShips();
 game.switchTurns();
 game.switchTurns();
--- src/UIController.js
+++ src/UIController.js
@@ -1,4 +1,43 @@
 // function to render board
-renderBoard() {
-    
+export function renderBoard(board) {
+    console.log('board:', board)
+
+    // define target for board
+    const app = document.querySelector("#app");
+    const boardElement = document.createElement("section")
+    boardElement.classList.add("board");
+
+
+    // set innerHTML to empty string
+    app.innerHTML = "";
+
+
+    // for loop for board length
+    const labelCollection = ["A","B","C","D","E","F","G","H","I","J"];
+    for (let i = 1; i < board.size + 1; i++) {
+
+        for (let j = 1; j < board.size + 1; j++) {
+            // build grid of cells
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+
+            // give data attributes that represent cell coordinates
+            cell.dataset.x = i;
+            cell.dataset.y = j;
+            
+            // visible label
+            cell.textContent = `${labelCollection[j - 1]}${i}`
+            
+            boardElement.append(cell);
+
+
+        }
+
+    }
+
+    // append labels to grid
+
+
+    // assembly
+    app.append(boardElement);
 }
